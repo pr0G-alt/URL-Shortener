7d0da6fb8b2ef6c3db50c22d66797a3bf7447c40
pr0.G <youssefmo5904@gmail.com>
2025-01-24 16:29:05 +0200

    created a route to get a single link data

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -11,3 +11,21 @@ exports.getAllLinks = (req, res) => {
     },
   });
 };
+
+exports.getLink = (req, res) => {
+  const link = links.find((link) => link.shortCode === req.params.shortCode);
+
+  if (!link) {
+    res.status(404).json({
+      status: 'fail',
+      message: `Link with short code "${req.params.shortCode}" does not exist.`,
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      link,
+    },
+  });
+};
--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -3,5 +3,6 @@ const linkController = require('./../controllers/linkController');
 const router = express.Router();
 
 router.get('/getAllLinks', linkController.getAllLinks);
+router.get('/getLink/:shortCode', linkController.getLink);
 
 module.exports = router;
