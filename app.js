89e2606b84fcdb36d846466e6fa9676a10c726f8
pr0.G <youssefmo5904@gmail.com>
2025-01-24 15:56:40 +0200

    created a route to get all links(data)

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
+});
+
+const links = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`));
+
+app.get('/getAllLinks', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: links.length,
+    data: {
+      links,
+    },
+  });
 });
 
 module.exports = app;
